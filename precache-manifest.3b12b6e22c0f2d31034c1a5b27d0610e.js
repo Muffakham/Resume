c7d9caf2e08c0539bd5d3455c4af4f5ff76ab819
unknown <muffakham.ali@gmail.com>
2020-05-19 08:56:43 -0700

    Updates

---
+++ precache-manifest.3b12b6e22c0f2d31034c1a5b27d0610e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8607acc256924dc6f4b7ca7dfd070d7e",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "f153bc1ebe2d0956c201",
+    "url": "/Resume/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "95b48576123cd673d2eb",
+    "url": "/Resume/static/js/2.1476ae79.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Resume/static/js/2.1476ae79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f153bc1ebe2d0956c201",
+    "url": "/Resume/static/js/main.5bad9c1b.chunk.js"
+  },
+  {
+    "revision": "f3187c13572c15a6eba5",
+    "url": "/Resume/static/js/runtime-main.2839ef2e.js"
+  }
+]);
